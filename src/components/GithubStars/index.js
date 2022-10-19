2b015d3ebbe71be337f3eb1d1c86e75360faf2e9
Elias <eliaxperez@gmail.com>
2022-10-19 10:55:48 -0300

    first commit

---
+++ src/components/GithubStars/index.js
@@ -0,0 +1,12 @@
+import GitHubButton from 'react-github-btn';
+import classnames from 'classnames';
+
+import styles from './GithubStars.module.css';
+
+const GithubStars = ({ className }) => (
+  <div className={classnames(styles.wrapper, className)}>
+
+  </div>
+);
+
+export default GithubStars;
---
+++ src/components/Layout/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classnames from 'classnames';
+
+
+import styles from './Layout.module.css';
+
+const Layout = ({ children, className }) => (
+  <div className={styles.pageWrapper}>
+    <div className={styles.border}>
+      <div className={classnames(styles.content, className)}>{children}</div>
+    </div>
+
+  </div>
+);
+
+export default Layout;
